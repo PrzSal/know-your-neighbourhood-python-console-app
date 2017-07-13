c8cda2e09d882f5e2ca0c92406c6eeb2dc8ca130
Przemo8922 <przemyslawsalak@gmail.com>
2017-07-13 21:29:13 +0200

    add docstring to Controller/data_controller.py and repair small bugs

--- Controller/data_controller.py
+++ Controller/data_controller.py
@@ -15,105 +15,135 @@ from Views.print_table import *
 
 
 def read_file():
-    with open('malopolska.csv') as f:
-        student = ''
-        reader = csv.reader(f, delimiter=',')
+    '''
+    Function that read data from csv file
+    and assigns to data to the appropriate class
+    ---------------------------------
+    Return:
+        None
+    '''
+    with open('malopolska.csv') as file_reader:
+        county_temporary = ''
+        reader = csv.reader(file_reader, delimiter=',')
         compare_location = CompareLocation()
+
         for row in reader:
             if 'województwo' in row:
                 Voivodeships(voivodeships=row[4], types=row[5])
+
             elif 'powiat' in row:
                 compare_location.remove_list_location()
-                NumberOfCommunity.add_county_and_number(student, NumberOfCommunity.number_community)
+                NumberOfCommunity.add_county_and_number(county_temporary, NumberOfCommunity.number_community)
+
                 if row[4] not in NumberOfCommunity.number_community_list:
-                    student = row[4]
+                    county_temporary = row[4]
+
                 compare_location.add_location(row[4], row[5])
                 County(county=row[4], types=row[5])
                 NumberOfCommunity.number_community = 0
+
             elif 'gmina miejska' in row:
                 CityMunicipality(city_municipality=row[4], types=row[5])
                 NumberOfCommunity()
                 compare_location.add_location(row[4], row[5])
+
             elif 'gmina wiejska' in row:
                 RuralCommune(rural_commune=row[4], types=row[5])
                 NumberOfCommunity()
                 compare_location.add_location(row[4], row[5])
+
             elif 'gmina miejsko-wiejska' in row:
                 RuralTown(rural_town=row[4], types=row[5])
                 NumberOfCommunity()
                 compare_location.add_location(row[4], row[5])
+
             elif 'obszar wiejski' in row:
                 RuralArea(rural_area=row[4], types=row[5])
                 NumberOfCommunity()
                 compare_location.add_location(row[4], row[5])
+
             elif 'miasto' in row:
                 City(city=row[4], types=row[5])
                 NumberOfCommunity()
                 compare_location.add_location(row[4], row[5])
+
             elif 'miasto na prawach powiatu' in row:
                 CityCounty(city_county=row[4], types=row[5])
                 NumberOfCommunity()
                 compare_location.add_location(row[4], row[5])
+
             elif 'delegatura' in row:
                 Delegacy(delegacy=row[4], types=row[5])
                 NumberOfCommunity()
                 compare_location.add_location(row[4], row[5])
+
             compare_location.compare_location()
 
 
 def list_statistic():
-    read_file()
-    title_list = ['location', 'types']
-#    list_ = Voivodeships.voivodeships_list
-    # sorted = False
-    # while not sorted:
-    #     sorted = True
-    #     for list_ in Voivodeships.voivodeships_list:
-    #         for step in range(len(list_)-1):
-    #             if len(list_[step][0]) > len(list_[step+1][0]):
-    #                 temporary = list_[step+1]
-    #                 list_[step+1] = list_[step]
-    #                 list_[step] = temporary
-    #                 sorted = False
-    print_compare_list(Voivodeships.voivodeships_list)
-    #print_table(list_to_table, title_list)
+    '''
+    Function that send list with object
+    to view module to display object
+    ---------------------------------
+    Return:
+        None
+    '''
+    print_statistic_list(Voivodeships.voivodeships_list)
 
 
 def three_large_city(list_):
-    read_file()
+    '''
+    Function sort list of object and
+    send list three city to print table
+    module
+    ---------------------------------
+    Return:
+        None
+    '''
     sorted = False
     while not sorted:
         sorted = True
+
         for step in range(len(list_)-1):
             if len(list_[step][0]) > len(list_[step+1][0]):
                 temporary = list_[step+1]
                 list_[step+1] = list_[step]
                 list_[step] = temporary
                 sorted = False
+
     title_list = ['location']
     location = []
+
     for i in range(len(list_)-3, len(list_)):
         location += [list_[i]]
     print_table(location, title_list)
 
 
 def county_with_the_largest_communities(list_):
-    read_file()
     sorted = False
     while not sorted:
         sorted = True
+
         for step in range(len(list_)-1):
             if int(list_[step][1]) > int(list_[step+1][1]):
                 temporary = list_[step+1]
                 list_[step+1] = list_[step]
                 list_[step] = temporary
                 sorted = False
+
     title_list = ['location', 'type']
     print_table(list_[-1:], title_list)
 
 
 def same_locations_in_more_one_category():
-    read_file()
+    '''
+    Function that read object with same
+    location and send list to print table
+    module
+    ---------------------------------
+    Return:
+        None
+    '''
     title_list = ['location']
     location = []
     for row in CompareLocation.same_location_list:
