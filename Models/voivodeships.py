bad71f7bc6e0d070a6fe8fa9e9f868fc057b9573
Przemo8922 <przemyslawsalak@gmail.com>
2017-07-13 21:39:58 +0200

    In Models/voivodeships.py  repair small bugs

--- Models/voivodeships.py
+++ Models/voivodeships.py
@@ -5,7 +5,9 @@ class Voivodeships:
     def __init__(self, voivodeships, types, county='po', city_municipality='gm',
                  rural_commune='gw', rural_town='omw',  rural_area='ow', city='m',
                  city_county='mnpp', delegacy='de'):
+
         Voivodeships.voivodeships_list.append(self)
+
         self.voivodeships = voivodeships
         self.county = county
         self.city_municipality = city_municipality
@@ -16,8 +18,9 @@ class Voivodeships:
         self.city_county = city_county
         self.delegacy = delegacy
         self.types = types
+
         self.__class__.quantity += 1
 
     def __str__(self):
-        string = '{}, type: {}'.format(Voivodeships.quantity, self.types)
+        string = '|   {} | {}               |'.format(Voivodeships.quantity, self.types)
         return string
