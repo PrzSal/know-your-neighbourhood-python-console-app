16ce5b9a89152acfa63db68f1ab944e6aa1691ec
Przemo8922 <przemyslawsalak@gmail.com>
2017-07-13 21:01:00 +0200

    add dockstring to compare_location and repair small bugs

--- Models/compare_location.py
+++ Models/compare_location.py
@@ -6,6 +6,13 @@ class CompareLocation:
         self.location_list = []
 
     def add_location(self, location, types):
+        """
+        Atribute:
+            location, types
+        Collect locations and types for comparison in tupple
+        Returns:
+            list of tupple
+        """
         self.location_list.append((location, types))
 
     def __eq__(self, location):
@@ -13,12 +20,28 @@ class CompareLocation:
         return self.get_perimeter() == location.get_perimeter()
 
     def add_same_location(self):
+        """
+        Save same locations for all user
+        Returns:
+            list
+        """
         __class__.same_location_list.append(self)
 
     def remove_list_location(self):
+        """
+        Data cleaning for new data in county
+        Returns:
+            empty list
+        """
         self.location_list = []
 
     def compare_location(self):
+        """
+        Compare location for one county and while find same location
+        save to list
+        Returns:
+            list or None
+        """
         try:
             for i in range(len(self.location_list)):
                 same_location = self.location_list[i][0]
