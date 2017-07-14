a56838e6025242fdfae39c9016dce5e9d6fb65dd
Przemo8922 <przemyslawsalak@gmail.com>
2017-07-14 13:14:41 +0200

    Refactor Modells/compare_location.py add list keep all locations

--- Models/compare_location.py
+++ Models/compare_location.py
@@ -1,11 +1,22 @@
 class CompareLocation:
-
+    temporary_location_list = []
     same_location_list = []
+    all_locations = []
 
-    def __init__(self):
-        self.temporary_location_list = []
+    def __init__(self, location='lo', types='typ'):
+        self.location = location
+        self.types = types
 
-    def add_location(self, location, types):
+    def __str__(self):
+        string = '| {}{} | {}{} |'.format(self.location, ' '*(23-len(self.location)),
+                                          self.types, ' '*(25-len(self.types)))
+        return string
+
+    def __eq__(self, location):
+        """Less-than comparison."""
+        return self.get_perimeter() == location.get_perimeter()
+
+    def add_location(self):
         """
         Atribute:
             location, types
@@ -13,11 +24,8 @@ class CompareLocation:
         Returns:
             list of tupple
         """
-        self.temporary_location_list.append((location, types))
-
-    def __eq__(self, location):
-        """Less-than comparison."""
-        return self.get_perimeter() == location.get_perimeter()
+        __class__.temporary_location_list.append((self.location, self.types))
+        __class__.all_locations.append(CompareLocation(self.location, self.types))
 
     def add_same_location(self):
         """
@@ -33,7 +41,7 @@ class CompareLocation:
         Returns:
             empty list
         """
-        self.location_list = []
+        __class__.temporary_location_list = []
 
     def compare_location(self):
         """
@@ -43,11 +51,10 @@ class CompareLocation:
             list or None
         """
         try:
-            for i in range(len(self.temporary_location_list)):
-                same_location = self.temporary_location_list[i][0]
-                type_location = self.temporary_location_list[i][1]
-
-                for location in self.temporary_location_list:
+            for i in range(len(__class__.temporary_location_list)):
+                same_location = __class__.temporary_location_list[i][0]
+                type_location = __class__.temporary_location_list[i][1]
+                for location in __class__.temporary_location_list:
                     if location[0] == same_location and location[1] != type_location:
                         if location[0] not in __class__.same_location_list:
                             __class__.add_same_location(location[0])
