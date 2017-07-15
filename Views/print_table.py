8068a2d23d66a780a8c4368cc5b281b86b8c7a8d
Przemo8922 <przemyslawsalak@gmail.com>
2017-07-15 16:58:03 +0200

    add docstring

--- Views/print_table.py
+++ Views/print_table.py
@@ -1,4 +1,12 @@
 def create_list_max_width_col(table):
+    '''
+    table (list in list)
+    Function that create list with max width colummn
+    ---------------------------------
+    Return:
+        List
+    '''
+    sorted = False
     ammount_of_column = len(table[0])
 
     max_elem_col = 0
@@ -14,18 +22,45 @@ def create_list_max_width_col(table):
 
 
 def print_insert_row_for_table(list_max_elem_col):
+    '''
+    Args:
+    list_max_elem_col (list)
+    Funcions print row between two data row
+    ---------------------------------
+    Return:
+        None
+    '''
+    sorted = False
     for elem in list_max_elem_col:
         print('|' + '-'*elem + '-'*4, end='')
     print('|')
 
 
 def print_row_name_column(title_list, list_max_elem_col):
+    '''
+    Args:
+    - tittle_list (list)
+    - list_max_elem_col (list)
+    Funcions print first row with tittle
+    ---------------------------------
+    Return:
+        None
+    '''
     for i in range(len(title_list)):
         print('|  ' + title_list[i] + ' '*(list_max_elem_col[i] - len(title_list[i]) + 2), end='')
     print('|')
 
 
 def print_content_from_table_in_row(table, list_max_elem_col):
+    '''
+    Args:
+    - table (list in list)
+    - list_max_elem_col (list)
+    Funcions print data row
+    ---------------------------------
+    Return:
+        None
+    '''
     for i in range(1, len(table)):
         print_insert_row_for_table(list_max_elem_col)
         for j in range(len(list_max_elem_col)):
@@ -34,7 +69,15 @@ def print_content_from_table_in_row(table, list_max_elem_col):
 
 
 def print_table(table, title_list):
-
+    '''
+    Args:
+    - table (list in list)
+    - tittle_list (list)
+    Funcions print all funcions with row
+    ---------------------------------
+    Return:
+        None
+    '''
     table = [title_list] + table
 
     list_max_elem_col = create_list_max_width_col(table)
