95747d13f565b299a957deabdcb12ffcf1019ec3
Przemo8922 <przemyslawsalak@gmail.com>
2017-07-15 17:07:51 +0200

    small change remove list in delegacy

--- Models/delegacy.py
+++ Models/delegacy.py
@@ -2,16 +2,11 @@ from Models.location import Location
 
 
 class Delegacy(Location):
-    delegacy_list = []
-
     quantity_d = 0
 
     def __init__(self, delegacy, types):
-
         self.__class__.quantity_d += 1
-
         super().__init__(delegacy, types)
-        Delegacy.delegacy_list.append(self)
 
     def __str__(self):
         string = '|   {} | {}                |'.format(__class__.quantity_d, self.types)
