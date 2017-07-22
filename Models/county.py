8344f7c55242a3596c214b99948750404a9324ac
Przemo8922 <przemyslawsalak@gmail.com>
2017-07-22 15:26:40 +0200

    repo test

--- Models/county.py
+++ Models/county.py
@@ -6,7 +6,7 @@ class County(Location):
 
     def __init__(self, county, types):
         self.__class__.quantity_c += 1
-        super().__init__(county, types)
+        super().__init__(countiy, types)
 
     def __str__(self):
         string = '|  {} | {}                    |'.format(__class__.quantity_c, self.types)
